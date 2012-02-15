f1fe820becabd0c61aae2fdf2d4218618fe90395
Steven Haryanto <stevenharyanto@gmail.com>
2012-02-15 10:28:03 +0000

    Import of SHARYANTO/Perinci-Sub-Wrapper-0.12 from CPAN.
    gitpan-cpan-distribution: Perinci-Sub-Wrapper
    gitpan-cpan-version:      0.12
    gitpan-cpan-path:         SHARYANTO/Perinci-Sub-Wrapper-0.12.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Test/Perinci/Sub/Wrapper.pm
+++ lib/Test/Perinci/Sub/Wrapper.pm
@@ -10,7 +10,7 @@ use Test::More 0.96;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(test_wrap);
 
-our $VERSION = '0.11'; # VERSION
+our $VERSION = '0.12'; # VERSION
 
 sub test_wrap {
     my %test_args = @_;
@@ -79,7 +79,7 @@ Test::Perinci::Sub::Wrapper
 
 =head1 VERSION
 
-version 0.11
+version 0.12
 
 =head1 AUTHOR
 
