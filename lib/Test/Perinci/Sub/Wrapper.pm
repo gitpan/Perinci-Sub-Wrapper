d8a53343df17026bfdbdaf5355fd867bc903e4a9
Steven Haryanto <stevenharyanto@gmail.com>
2013-03-12 19:08:28 +0000

    Import of SHARYANTO/Perinci-Sub-Wrapper-0.40 from CPAN.
    gitpan-cpan-distribution: Perinci-Sub-Wrapper
    gitpan-cpan-version:      0.40
    gitpan-cpan-path:         SHARYANTO/Perinci-Sub-Wrapper-0.40.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Test/Perinci/Sub/Wrapper.pm
+++ lib/Test/Perinci/Sub/Wrapper.pm
@@ -10,7 +10,7 @@ use Test::More 0.96;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(test_wrap);
 
-our $VERSION = '0.39'; # VERSION
+our $VERSION = '0.40'; # VERSION
 
 sub test_wrap {
     my %test_args = @_;
@@ -138,19 +138,11 @@ Test::Perinci::Sub::Wrapper - Provide test_wrap() to test wrapper
 
 =head1 VERSION
 
-version 0.39
+version 0.40
 
 =for Pod::Coverage test_wrap
 
-=head1 DESCRIPTION
-
-
-This module has L<Rinci> metadata.
-
 =head1 FUNCTIONS
-
-
-None are exported by default, but they are exportable.
 
 =head1 AUTHOR
 
